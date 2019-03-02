341a43b2d95bce72e3d2e199c3c2098947d45250
Preston <freetubeapp@protonmail.com>
2019-03-01 22:23:28 -0500

    Remove useless curl statement

--- src/index.php
+++ src/index.php
@@ -5,20 +5,6 @@ $url = "https://api.github.com/repos/freetubeapp/freetube/releases";
 
 $curl = curl_init();
 
-switch ($method) {
-    case "POST":
-        curl_setopt($curl, CURLOPT_POST, 1);
-        if ($data)
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        break;
-    case "PUT":
-        curl_setopt($curl, CURLOPT_PUT, 1);
-        break;
-    default:
-        if ($data)
-            $url = sprintf("%s?%s", $url, http_build_query($data));
-}
-
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
