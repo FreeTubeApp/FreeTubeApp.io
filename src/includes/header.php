f0f3d8c912c66ada3adf9a6e93529196cf74b5a4
Preston <freetubeapp@protonmail.com>
2019-03-01 22:12:59 -0500

    Update Content Headers

--- src/includes/header.php
+++ src/includes/header.php
@@ -14,7 +14,9 @@ else {
 <html>
 
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style/main.css">
   <link rel="stylesheet" href="style/solid.min.css">
   <link rel="stylesheet" href="style/brands.min.css">
