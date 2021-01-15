5c4156e91d17e0aa4158ffc745afeb4bc50ddedb
Preston <freetubeapp@protonmail.com>
2021-01-15 17:20:18 -0500

    Bump version number to v0.11.1

--- src/index.php
+++ src/index.php
@@ -1,11 +1,11 @@
 <?php
 require 'includes/header.php';
 
-$currentRelease = 'Release 0.11.0 Beta';
+$currentRelease = 'Release 0.11.1 Beta';
 
-$fullTagName = 'v0.11.0-beta';
+$fullTagName = 'v0.11.1-beta';
 
-$versionNumber = '0.11.0';
+$versionNumber = '0.11.1';
 
 $baseUrl = "https://github.com/FreeTubeApp/FreeTube/releases/download/";
 
