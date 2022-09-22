bfbc5f3ff7f9000de07ce972c37ca362ca3137ed
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2022-09-22 04:57:40 -0400

    improve accessibility (#21)
    * improve accessibility
    
    * Improve text for links
    
    Co-Authored-By: PikachuEXE <pikachuexe@gmail.com>
    
    Co-authored-by: PikachuEXE <pikachuexe@gmail.com>

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -2,27 +2,27 @@
     <div class="px-5 container mx-auto flex items-center justify-between">
       <div>
         <p class="py-3">This site is released under the MIT license.</p>
-        <p class="py-2"> The source code for the site can be found <a href='http://github.com/FreeTubeApp/FreeTubeApp.io' class="text-gray-200">here</a>.</p>
+        <p class="py-2"> The source code for the site can be found on the <a href='http://github.com/FreeTubeApp/FreeTubeApp.io' class="text-gray-200">FreeTubeApp.io GitHub Repo</a>.</p>
       </div>
       <div>
         <nav class="flex">
-          <a href='https://github.com/FreeTubeApp/FreeTube' title="Github Repository" aria-label="Github Repository">
+          <a href='https://github.com/FreeTubeApp/FreeTube' title="GitHub Repository" aria-label="GitHub Repository">
             <i class="fab fa-github icon"></i>
           </a>
-          <a href='https://matrix.to/#/+freetube:matrix.org' title="Chat with Matrix" aria-label="">
+          <a href='https://matrix.to/#/+freetube:matrix.org' title="Chat with Matrix" aria-label="Chat with Matrix">
             <i class="fas fa-comment icon"></i>
           </a>
-          <a href='https://blog.freetubeapp.io' title="Blog" aria-label="">
+          <a href='https://blog.freetubeapp.io' title="Blog" aria-label="Blog">
             <i class="fas fa-blog icon"></i>
           </a>
-          <a href='https://docs.freetubeapp.io' title="Documentation" aria-label="">
+          <a href='https://docs.freetubeapp.io' title="Documentation" aria-label="Documentation">
             <i class="fas fa-file-alt icon"></i>
           </a>
-          <a href='https://liberapay.com/FreeTube' title="Donate" aria-label="">
+          <a href='https://liberapay.com/FreeTube' title="Donate" aria-label="Donate">
             <i class="fas fa-donate icon"></i>
           </a>
         </nav>
-        <p class="mt-3">Copyright &copy; FreeTube 2021</p>
+        <p class="mt-3">Copyright &copy; FreeTube 2022</p>
       </div>
     </div>
   </footer><!-- /footer -->
