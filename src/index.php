aff77e6e0531a3e8770c7170032c672c5511a4e1
Preston <freetubeapp@protonmail.com>
2024-10-26 21:30:42 -0400

    Bump version number to v0.22.0

--- src/index.php
+++ src/index.php
@@ -1,11 +1,11 @@
 <?php
 require 'includes/header.php';
 
-$currentRelease = 'Release 0.21.3 Beta';
+$currentRelease = 'Release 0.22.0 Beta';
 
-$fullTagName = 'v0.21.3-beta';
+$fullTagName = 'v0.22.0-beta';
 
-$versionNumber = '0.21.3';
+$versionNumber = '0.22.0';
 
 $baseUrl = "https://github.com/FreeTubeApp/FreeTube/releases/download/";
 
