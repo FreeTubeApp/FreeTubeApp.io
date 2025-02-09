2d09b5df4656964dbb53c416d78172667340364b
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2025-02-09 17:12:54 +0100

    add icon

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -7,16 +7,16 @@
       <div>
         <nav class="flex">
           <a href='https://github.com/FreeTubeApp/FreeTube' title="GitHub Repository" aria-label="GitHub Repository">
-            <i class="fa-brands fa-github text-4xl"></i>
+            <i class="fa-brands fa-github icon"></i>
           </a>
           <a href='https://matrix.to/#/+freetube:matrix.org' title="Chat with Matrix" aria-label="Chat with Matrix">
-            <i class="fa-solid fa-comment text-4xl"></i>
+            <i class="fa-solid fa-comment icon"></i>
           </a>
           <a href='https://blog.freetubeapp.io' title="Blog" aria-label="Blog">
-            <i class="fa-solid fa-blog text-4xl"></i>
+            <i class="fa-solid fa-blog icon"></i>
           </a>
           <a href='https://docs.freetubeapp.io' title="Documentation" aria-label="Documentation">
-            <i class="fa-solid fa-file-lines text-4xl"></i>
+            <i class="fa-solid fa-file-lines icon"></i>
           </a>
         </nav>
         <p class="mt-3">Copyright &copy; FreeTube 2025</p>
