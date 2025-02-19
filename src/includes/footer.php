9dcb051f39ef3233beafce43b21bee390077dd4c
Antonin Del Fabbro <message@antonin.one>
2025-02-19 23:04:00 +0100

    Fixed my fix (…)

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -2,20 +2,20 @@
     <div class="px-5 container mx-auto flex items-center justify-between">
       <div>
         <p class="py-3">This site is released under the MIT license.</p>
-        <p class="py-2"> The source code for the site can be found on the <a href="http://github.com/FreeTubeApp/FreeTubeApp.io" "class="text-gray-200">FreeTubeApp.io GitHub Repo</a>.</p>
+        <p class="py-2">The source code for the site can be found on the <a href="http://github.com/FreeTubeApp/FreeTubeApp.io" class="text-gray-200">FreeTubeApp.io GitHub repository</a>.</p>
       </div>
       <div>
         <nav class="flex">
-          <a href="https://github.com/FreeTubeApp/FreeTube" "title="GitHub Repository" aria-label="GitHub Repository">
+          <a href="https://github.com/FreeTubeApp/FreeTube" title="GitHub Repository" aria-label="GitHub Repository">
             <i class="fab fa-github icon"></i>
           </a>
-          <a href="https://matrix.to/#/#freetube:matrix.org" "title="Chat with Matrix" aria-label="Chat with Matrix">
+          <a href="https://matrix.to/#/#freetube:matrix.org" title="Chat with Matrix" aria-label="Chat with Matrix">
             <i class="fas fa-comment icon"></i>
           </a>
-          <a href="https://blog.freetubeapp.io" "title="Blog" aria-label="Blog">
+          <a href="https://blog.freetubeapp.io" title="Blog" aria-label="Blog">
             <i class="fas fa-blog icon"></i>
           </a>
-          <a href="https://docs.freetubeapp.io" "title="Documentation" aria-label="Documentation">
+          <a href="https://docs.freetubeapp.io" title="Documentation" aria-label="Documentation">
             <i class="fas fa-file-alt icon"></i>
           </a>
         </nav>
