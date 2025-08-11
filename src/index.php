c01cfa1aa62dc0622f4517d433b360509b191cf5
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2025-08-11 05:08:58 +0200

    prioritize arm architecture (#72)

--- src/index.php
+++ src/index.php
@@ -206,15 +206,6 @@ $downloadPacmanx64 = $baseUrl.$fullTagName."/freetube-".$versionNumber."-beta-am
             </div>
             <div class="ml-8">
               <h3 class="p-1 text-2xl">macOS</h3>
-              <a href="<?php echo $downloadMacDmg; ?>">
-                <span class="download">.dmg (x64)</span>
-              </a>
-              <a href="<?php echo $downloadMacZip; ?>">
-                <span class="download">.zip (x64)</span>
-              </a>
-              <a href="<?php echo $downloadMac7z; ?>">
-                <span class="download">.7z (x64)</span>
-              </a>
               <a href="<?php echo $downloadMacArm; ?>">
                 <span class="download">.dmg (arm64)</span>
               </a>
@@ -223,6 +214,15 @@ $downloadPacmanx64 = $baseUrl.$fullTagName."/freetube-".$versionNumber."-beta-am
               </a>
               <a href="<?php echo $downloadMacArm7z; ?>">
                 <span class="download">.7z (arm64)</span>
+              </a>
+              <a href="<?php echo $downloadMacDmg; ?>">
+                <span class="download">.dmg (x64)</span>
+              </a>
+              <a href="<?php echo $downloadMacZip; ?>">
+                <span class="download">.zip (x64)</span>
+              </a>
+              <a href="<?php echo $downloadMac7z; ?>">
+                <span class="download">.7z (x64)</span>
               </a>
             </div>
           </div>
