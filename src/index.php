82eea60f65110c5e1fa790644b8785bf92e6fbab
efb4f5ff-1298-471a-8973-3d47447115dc <73130443+efb4f5ff-1298-471a-8973-3d47447115dc@users.noreply.github.com>
2025-08-23 16:26:52 +0200

    Bump version number to v0.23.8 (#75)

--- src/index.php
+++ src/index.php
@@ -1,11 +1,11 @@
 <?php
 require 'includes/header.php';
 
-$currentRelease = 'Release 0.23.7 Beta';
+$currentRelease = 'Release 0.23.8 Beta';
 
-$fullTagName = 'v0.23.7-beta';
+$fullTagName = 'v0.23.8-beta';
 
-$versionNumber = '0.23.7';
+$versionNumber = '0.23.8';
 
 $baseUrl = "https://github.com/FreeTubeApp/FreeTube/releases/download/";
 
